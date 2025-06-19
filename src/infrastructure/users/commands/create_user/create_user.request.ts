ed931cac64e6c361aef2ea41078faac4ef9260c1
Dani <danielg2964@gmail.com>
2025-06-18 23:58:11 -0500

    update: Typebox Schema

--- src/infrastructure/users/commands/create_user/create_user.request.ts
+++ src/infrastructure/users/commands/create_user/create_user.request.ts
@@ -2,7 +2,7 @@ import { Type, type Static } from "@sinclair/typebox";
 
 export const CreateUserRequest = Type.Object({
   email: Type.String({ format: "email" }),
-  password: Type.String()
+  password: Type.String({ minLength: 8 })
 });
 
 export type CreateUserRequest = Static<typeof CreateUserRequest>
